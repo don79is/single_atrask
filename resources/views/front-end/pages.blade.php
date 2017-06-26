29bc4f20bdc53f49d35efdcb878b17fc033e6a1a
don79is <don79is@gmail.com>
2017-06-26 12:39:34 +0300

    show front end image

--- resources/views/front-end/pages.blade.php
+++ resources/views/front-end/pages.blade.php
@@ -7,7 +7,7 @@
         {{$title}}
     </div>
     <div>
-        <img src="{{$pages['image']['path']}}">
+        <img src="{{asset ($pages['image']['path'])}}" class="image-size">
     </div>
     <h4>{{$description_long}}</h4>
 @endsection
