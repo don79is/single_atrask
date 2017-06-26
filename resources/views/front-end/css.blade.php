c19bd71961cf491baa9e51eecbb22e8288199857
don79is <don79is@gmail.com>
2017-06-26 12:39:47 +0300

    front end css

--- resources/views/front-end/css.blade.php
+++ resources/views/front-end/css.blade.php
@@ -1,2 +1,4 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
-<link href="/css/front-endCss.css" rel=stylesheet>
+
+<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('css/front-endCss.css') }}">
