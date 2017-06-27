db071b049e739a7accac29ddc8f50a321c653dbe
don79is <don79is@gmail.com>
2017-06-27 23:00:04 +0300

    fix

--- resources/views/admin/adminCore.blade.php
+++ resources/views/admin/adminCore.blade.php
@@ -15,6 +15,7 @@
 @yield('list')
 @yield('form')
 </body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 @yield('scripts')
 </html>
